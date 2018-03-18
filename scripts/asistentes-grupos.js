46b48b7ebc7689b20709542b15bdb1a522b6add5
Luis Artola <vanessa@macbook-pro-de-vanessa.local>
2018-03-18 12:21:47 +0100

    grpos

--- scripts/asistentes-grupos.js
+++ scripts/asistentes-grupos.js
@@ -49,7 +49,7 @@ const entryToMarkdown = e => {
 };
 
 const asistentesGruposHtml = grupos.map(grupo => {
-    return `<li><a href='asistentes-grupos/grupo${grupo.id}.html'>${grupo.name}</a></li>`;
+    return `<li><a href='/asistentes-grupos/${grupo.id}.html'>${grupo.name}</a></li>`;
 }).reduce((a,b) => a+b, '');
 
 fs.writeFileSync('../asistentes-grupos/grupos.html', `<ul>${asistentesGruposHtml}</ul>`);
