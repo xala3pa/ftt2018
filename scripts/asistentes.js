92833f6b87bff1e733b428e5db340edc213b477e
Luis Artola <vanessa@macbook-pro-de-vanessa.local>
2018-03-18 11:41:06 +0100

    asistentes

---
+++ scripts/asistentes.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const entryToMarkdown = e => {
+    return `<li> 
+ ${e.gsx$nombre.$t}
+ Twitter : ${e.gsx$twitter.$t}
+ Foto: ${e.gsx$foto.$t}
+</li>
+`;
+};
+
+const asistentes = JSON.parse(fs.readFileSync(__dirname + '/asistentes.json', 'utf8'));
+const asistentesHtml  = asistentes.feed.entry
+    .map(entryToMarkdown)
+    .reduce((a,b) => a+b, '');
+
+fs.writeFileSync("../asistentes.html", `<ul>${asistentesHtml}</ul>`);
--- scripts/get-spreadsheet.sh
+++ scripts/get-spreadsheet.sh
@@ -1,2 +1,6 @@
 wget https://spreadsheets.google.com/feeds/list/1O69PulSKrQO2vFkpb19GJBYN7C7aCjFgiXGYIefAVqA/og2i2rn/public/values?alt=json
 mv values\?alt=json ftt-data.json
+wget https://spreadsheets.google.com/feeds/list/1O69PulSKrQO2vFkpb19GJBYN7C7aCjFgiXGYIefAVqA/2/public/values?alt=json
+mv values\?alt=json asistentes.json
+
+
