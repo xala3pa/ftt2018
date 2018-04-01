7b6d3f50ffc3bd272230b376820d9aef380d703e
Luis Artola <vanessa@macbook-pro-de-vanessa.local>
2018-04-01 16:11:30 +0200

    coco

--- scripts/asistentes.js
+++ scripts/asistentes.js
@@ -1,26 +1,15 @@
 const fs = require('fs');
 
-const entryToMarkdown = e => {
-    const a = `- ${e.gsx$nombre.$t}`;
-    const b = `- Twitter : [${e.gsx$twitter.$t}](https://twitter.com/${e.gsx$twitter.$t})`;
-    const c = `- [Experiencia](http://ftt.programania.net/experiencias/${e.gsx$id.$t}.html) `;
-    const d = `- ![Foto](${e.gsx$foto.$t})`;
-    return a+"\n"+b+"\n"+c+"\n"+d+"\n";
-};
-
-const entryToHtml = e => `
+const asistentes = JSON.parse(fs.readFileSync(__dirname + '/asistentes.json', 'utf8'));
+const asistentesHtml  = asistentes.feed.entry
+    .map(e => `
 <div class="card">
   <img src="${e.gsx$foto.$t}" alt="Avatar" style="width:100%">
   <div class="container">
     <h4><b>${e.gsx$nombre.$t}</b></h4> 
-    <p>Architect & Engineer</p> 
+    <p><a href="/experiencias/${e.gsx$id.$t}.html">experiencia ${e.gsx$id.$t}</a></p> 
   </div>
 </div>
-`;
-
-const asistentes = JSON.parse(fs.readFileSync(__dirname + '/asistentes.json', 'utf8'));
-const asistentesHtml  = asistentes.feed.entry
-    .map(entryToHtml)
-    .reduce((a,b) => a+b, '');
+`).reduce((a,b) => a+b, '');
 
 fs.writeFileSync("../asistentes/asistentes.md", `${asistentesHtml}`);
