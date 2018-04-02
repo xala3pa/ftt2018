7b78fcfca66c1e5228aa21f9c36be82640d7a5ec
Luis Artola <vanessa@macbook-pro-de-vanessa.local>
2018-04-02 10:22:18 +0200

    coco

--- scripts/asistentes.js
+++ scripts/asistentes.js
@@ -7,8 +7,8 @@ const asistentesHtml  = asistentes.feed.entry
   <img src="${e.gsx$foto.$t}" alt="Avatar" style="width:100%">
   <div class="container">
     <h4><b>${e.gsx$nombre.$t}</b></h4> 
-    <p><a href="/experiencias/${e.gsx$id.$t}.html">experiencia ${e.gsx$id.$t}</a> 
-    <a href="/asistentes-grupos/grupo${e.gsx$grupo.$t}.html">Grupo ${e.gsx$grupo.$t}</a></p> 
+    <p><a href="/experiencias/${e.gsx$id.$t}.html">experiencia ${e.gsx$id.$t}</a>, 
+    <a href="/asistentes-grupos/grupo${e.gsx$grupo.$t}.html">grupo ${e.gsx$grupo.$t}</a></p> 
   </div>
 </div>
 `).reduce((a,b) => a+b, '');
