e72b51fa714731eeb5e643a9ba3f80498438636c
luis <luisartola1@gmail.com>
2018-04-13 09:13:44 +0200

    coco

---
+++ scripts/asistentes-tabla.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const asistentes = JSON.parse(fs.readFileSync(__dirname + '/asistentes.json', 'utf8'));
+const asistentesHtml  = asistentes.feed.entry
+    .map(e => `
+
+<tr>
+<td style="border-bottom:thin solid black">
+  <img src="${e.gsx$foto.$t}" alt="Avatar" style="width:40px">
+</td>
+<td style="border-bottom:thin solid black">
+<b>${e.gsx$nombre.$t}</b>
+</td>
+<td style="border-bottom:thin solid black">${e.gsx$grupo.$t}</td>
+<td style="border-bottom:thin solid black"></td>
+</td>
+`).reduce((a,b) => a+b, '');
+
+fs.writeFileSync("../asistentes/asistentes-tabla.html", `
+
+
+<table style="width:90%;margin-left:10px;">
+<thead>
+<tr>
+<th>foto</th>
+<th>nombre</th>
+<th>grupo</th>
+<th>desvirtualizado</th>
+</tr>
+</thead>
+<tbody>
+
+${asistentesHtml + ''}
+</tbody>
+</table>
+
+`);
